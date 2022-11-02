35363eb27c318a92160b222d69d540108b46228c
Albert Toshiro Heru <alberttoshiro.heru@gmail.com>
2022-11-02 11:37:55 +0700

    First Commit

---
+++ hibernate-introduction/src/main/java/com/albert/hibernate/App.java
@@ -0,0 +1,13 @@
+package com.albert.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
